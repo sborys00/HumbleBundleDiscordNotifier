bea35640f052386a3307040a3b56a676f6476938
sborys00 <16275683+sborys00@users.noreply.github.com>
2020-11-08 19:46:43 +0100

    add Product class

---
+++ HumbleBundleDiscordNotifier/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace HumbleBundleDiscordNotifier.Models
+{
+    class Product
+    {
+        [JsonPropertyName("machine_name")]
+        public string MachineName { get; set; }
+
+        [JsonPropertyName("product_url")]
+        public string ProductUrl { get; set; }
+
+        [JsonPropertyName("tile_name")]
+        public string ProductName { get; set; }
+
+        [JsonPropertyName("high_res_tile_image")]
+        public string ProductThumbnailUrl { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("start_date|datetime")]
+        public DateTime StartDate { get; set; }
+
+        [JsonPropertyName("end_date|datetime")]
+        public DateTime EndDate { get; set; }
+    }
+}
