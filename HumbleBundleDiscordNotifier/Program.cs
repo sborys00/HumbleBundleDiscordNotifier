1dca56e30728f3e4c5234cf347d28ae304a48a46
sborys00 <16275683+sborys00@users.noreply.github.com>
2020-11-08 15:55:41 +0100

    Add project files.

---
+++ HumbleBundleDiscordNotifier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HumbleBundleDiscordNotifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
