c9ff45a113797e884723c06a0d82f67b0e10d4b7
sborys00 <16275683+sborys00@users.noreply.github.com>
2020-11-08 21:31:45 +0100

    add Scraper class

---
+++ HumbleBundleDiscordNotifier/Models/IScraper.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace HumbleBundleDiscordNotifier.Models
+{
+    interface IScraper
+    {
+        List<Product> GetListOfProducts();
+    }
+}
---
+++ HumbleBundleDiscordNotifier/Models/Scraper.cs
@@ -0,0 +1,45 @@
+﻿using HtmlAgilityPack;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Core;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace HumbleBundleDiscordNotifier.Models
+{
+    public class Scraper : IScraper
+    {
+        private readonly IConfiguration _config;
+        private readonly string url;
+
+        public Scraper(IConfiguration config)
+        {
+            _config = config;
+            url = config.GetValue<string>("WebsiteUrl");
+        }
+
+        public List<Product> GetListOfProducts()
+        {
+            HtmlDocument doc = DownloadHtmlDocument();
+            string data = ExtractDataFromHtmlDocument(doc);
+            JsonDocument json = JsonDocument.Parse(data);
+            JsonElement productsJson = json.RootElement.GetProperty("mosaic")[1].GetProperty("products");
+            List<Product> products = JsonSerializer.Deserialize<List<Product>>(productsJson.GetRawText());
+            return products;
+        }
+
+        private HtmlDocument DownloadHtmlDocument()
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load(url);
+            return document;
+        }
+
+        private string ExtractDataFromHtmlDocument(HtmlDocument doc)
+        {
+            string data = doc.GetElementbyId("webpack-json-data").InnerText;
+            return data;
+        }
+    }
+}
