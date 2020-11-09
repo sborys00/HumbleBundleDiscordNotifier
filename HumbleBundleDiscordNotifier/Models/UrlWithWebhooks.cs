e0b191da1e8fb22f0ec98dd32383fac5eda599cb
julek64 <juliusz644@gmail.com>
2020-11-09 10:16:34 +0100

    add UrlWithWebhooks class

---
+++ HumbleBundleDiscordNotifier/Models/UrlWithWebhooks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumbleBundleDiscordNotifier.Models
+{
+    class UrlWithWebhooks
+    {
+        public string Url { get; set; }
+        public List<Webhook> Webhooks { get; set; }
+
+        public UrlWithWebhooks(string url, List<Webhook> webhooks)
+        {
+            Url = url;
+            Webhooks = new List<Webhook>(webhooks);
+        }
+    }
+}
