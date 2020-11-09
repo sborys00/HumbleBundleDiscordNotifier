2dc68a408dc186e1b68c9d08efbaa117d663ab94
julek64 <juliusz644@gmail.com>
2020-11-09 10:16:15 +0100

    add Webhook class

---
+++ HumbleBundleDiscordNotifier/Models/Webhook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumbleBundleDiscordNotifier.Models
+{
+    class Webhook
+    {
+        public string Hash { get; set; }
+        private string url;
+
+        public Webhook(string url)
+        {
+            this.url = url;
+            //Hash = hash the url
+        }
+    }
+}
