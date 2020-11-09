c904aa0510237535b9f812acdce4994cc36e3d52
julek64 <juliusz644@gmail.com>
2020-11-09 11:09:07 +0100

    add creation of hash string in Webhook class

--- HumbleBundleDiscordNotifier/Models/Webhook.cs
+++ HumbleBundleDiscordNotifier/Models/Webhook.cs
@@ -1,18 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace HumbleBundleDiscordNotifier.Models
 {
     class Webhook
     {
-        public string Hash { get; set; }
-        private string url;
+        public readonly string Hash;
+        private readonly string url;
 
         public Webhook(string url)
         {
             this.url = url;
-            //Hash = hash the url
+            Hash = GetHashString(url);
+        }
+
+        private byte[] GetHash(string input)
+        {
+            using (HashAlgorithm algorithm = SHA256.Create())
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+        }
+
+        private string GetHashString(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(input))
+                sb.Append(b.ToString("X2"));
+
+            return sb.ToString();
         }
     }
 }
