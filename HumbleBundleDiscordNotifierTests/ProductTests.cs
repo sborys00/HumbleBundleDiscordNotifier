b997c87cc6ca2367f166119ebe205087aa70eab0
sborys00 <16275683+sborys00@users.noreply.github.com>
2020-11-10 11:48:18 +0100

    add deserialization test for Product class

---
+++ HumbleBundleDiscordNotifierTests/ProductTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.IO;
+using HumbleBundleDiscordNotifier.Models;
+using System.Text.Json;
+
+namespace HumbleBundleDiscordNotifierTests
+{
+    public class ProductTests
+    {
+        private readonly string _productsJson;
+
+        public ProductTests()
+        {
+            //loads example products
+            using (StreamReader sr = new StreamReader(Directory.GetCurrentDirectory() + "/TestJsonFiles/ExampleProducts.json"))
+            {
+                _productsJson = sr.ReadToEnd();                
+            }
+        }
+
+        [Fact]
+        public void CanBeDeserialized()
+        {
+            List<Product> products = JsonSerializer.Deserialize<List<Product>>(_productsJson);
+        }
+    }
+}
