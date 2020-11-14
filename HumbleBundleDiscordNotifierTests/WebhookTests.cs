2dc2dc237644de0b835cb1a01fe40392d08ee75d
sborys00 <16275683+sborys00@users.noreply.github.com>
2020-11-14 18:57:33 +0100

    add WebhookTests

---
+++ HumbleBundleDiscordNotifierTests/WebhookTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using HumbleBundleDiscordNotifier.Models;
+
+namespace HumbleBundleDiscordNotifierTests
+{
+    public class WebhookTests
+    {
+        [Fact]
+        public void GenerateWebhooks_ShouldWork()
+        {
+            string[] urls = {
+                "https://www.domain.com/test1",
+                "https://www.domain.com/test2",
+                "https://www.domain.com/test3",
+                "https://www.domain.com/test4",
+                "https://www.domain.com/test5",
+                "https://www.domain.com/test6",
+            };
+            List<Webhook> webhooks = Webhook.GenerateWebhooks(urls);
+            for (int i = 0; i < urls.Length; i++)
+            {
+                Assert.Equal(urls[i], webhooks[i].url);
+            }
+        }
+    }
+}
