b3dcb12ad4099bd0df6a4b270bd6bc13e4950cde
sborys00 <16275683+sborys00@users.noreply.github.com>
2020-11-17 20:41:52 +0100

    add PostAsync to ICustomWebClient

--- HumbleBundleDiscordNotifier/Models/CustomWebClient.cs
+++ HumbleBundleDiscordNotifier/Models/CustomWebClient.cs
@@ -1,4 +1,6 @@
 ﻿using HtmlAgilityPack;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace HumbleBundleDiscordNotifier.Models
 {
@@ -9,6 +11,14 @@ namespace HumbleBundleDiscordNotifier.Models
             HtmlWeb web = new HtmlWeb();
             HtmlDocument doc = web.Load(url);
             return doc.Text;
+        }
+
+        public async Task<HttpResponseMessage> PostAsync(string url, StringContent payload)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                return await httpClient.PostAsync(url, payload);
+            }
         }
     }
 }
--- HumbleBundleDiscordNotifier/Models/ICustomWebClient.cs
+++ HumbleBundleDiscordNotifier/Models/ICustomWebClient.cs
@@ -1,7 +1,11 @@
-﻿namespace HumbleBundleDiscordNotifier.Models
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HumbleBundleDiscordNotifier.Models
 {
     public interface ICustomWebClient
     {
         string GetWebsite(string url);
+        Task<HttpResponseMessage> PostAsync(string url, StringContent payload);
     }
 }
